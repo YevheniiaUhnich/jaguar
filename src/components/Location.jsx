3d913945733a80e54e760b306b3763a8ca070e65
Yevheniia Uhnich <kjaine@gmail.com>
2025-10-31 14:12:21 +0200

    mapa

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -2,15 +2,26 @@ import React from 'react'
 import '../styles/Location.css'
 
 const Location = () => {
+
+  const address = 'вул. Набережна Заводська, 53, Дніпро';
+  const src = `https://www.google.com/maps?q=${encodeURIComponent(address)}&hl=uk&z=15&output=embed`;
+
   return (
     <section id="location" className="location">
       <div className="container">
         <h2 className="section-title">Наша локація</h2>
         <div className="location-content">
           <div className="location-map">
-            {/* Тут буде карта або зображення локації */}
-            <div className="placeholder-map">
-              <p>Карта розташування</p>
+
+            <div className="map-wrap">
+              <iframe
+                className="map-iframe"
+                src={src}
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                allowFullScreen
+                title="Карта розташування"
+              />
             </div>
           </div>
           <div className="location-info">
@@ -19,12 +30,12 @@ const Location = () => {
               <p>вул. Набережна Заводська, 53, м. Дніпро</p>
 
               <div className="info-item">
-              <h3>Контакти</h3>
-              <p>Телефон: +380 (00) 111-11-11</p>
-              <p>Email: info@sportsclub.ua</p>
+                <h3>Контакти</h3>
+                <p>Телефон: +380 (00) 111-11-11</p>
+                <p>Email: info@sportsclub.ua</p>
+              </div>
             </div>
-            </div>
-            
+
             <div className="info-item">
               <h3>Графік роботи</h3>
               <p>Понеділок: 16:00 - 20:00</p>
@@ -34,9 +45,9 @@ const Location = () => {
               <p>Пʼятниця: 6:00 - 80:00</p>
               <p>Субота, неділя: Ігри ЧУ</p>
             </div>
-            
-            
-            
+
+
+
           </div>
         </div>
       </div>
