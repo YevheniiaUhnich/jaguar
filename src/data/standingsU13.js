a180ed4609800271fabd16431c97daaecf87b4b7
Yevheniia Uhnich <kjaine@gmail.com>
2025-11-01 09:36:41 +0200

    data

--- src/data/standingsU13.js
+++ src/data/standingsU13.js
@@ -1,11 +1,11 @@
 export const standingsU13 = [
-    { place: 0, team: 'МДЮСШ Ягуари, м. Дніпро',            games: 2, zsh: 0, psh: 0, vi: 0, pi: 0, pts: 0 },
-    { place: 0, team: 'ДЮСШ Сокіл 1, м. Київ',              games: 2, zsh: 0, psh: 0, vi: 0, pi: 0, pts: 0 },
+    { place: 0, team: 'МДЮСШ Ягуари, м. Дніпро',            games: 2, zsh: 3, psh: 20, vi: 0, pi: 2, pts: 0 },
+    { place: 0, team: 'ДЮСШ Сокіл 1, м. Київ',              games: 2, zsh: 0, psh: 0, vi: 2, pi: 0, pts: 6 },
     { place: 0, team: 'ДЮСШ Сокіл 2, м. Київ',              games: 2, zsh: 0, psh: 0, vi: 0, pi: 0, pts: 0 },
     { place: 0, team: 'ДЮСШ Крижинка, м. Київ',             games: 2, zsh: 0, psh: 0, vi: 0, pi: 0, pts: 0 },
     { place: 0, team: 'МДЮСШ Дніпро, м. Дніпро',            games: 2, zsh: 0, psh: 0, vi: 0, pi: 0, pts: 0 },
-    { place: 0, team: 'ДЮСШ Авангард, м. Кременчук',        games: 2, zsh: 0, psh: 0, vi: 0, pi: 0, pts: 0 },
-    { place: 0, team: 'ДЮСШ Патріот, м. Вінниця',           games: 2, zsh: 0, psh: 0, vi: 0, pi: 0, pts: 0 },
+    { place: 0, team: 'ДЮСШ Авангард, м. Кременчук',        games: 2, zsh: 0, psh: 0, vi: 0, pi: 2, pts: 0 },
+    { place: 0, team: 'ДЮСШ Патріот, м. Вінниця',           games: 2, zsh: 20, psh: 3, vi: 2, pi: 0, pts: 6 },
     { place: 0, team: 'КПНЗ КОР КОК ДЮСШ Шершні, м. Богуслав', games: 2, zsh: 0, psh: 0, vi: 0, pi: 0, pts: 0 },
   ];
   
