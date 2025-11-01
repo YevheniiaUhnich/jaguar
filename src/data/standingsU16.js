0fc98eb60555225169f03f11615c47c4098c2040
Yevheniia Uhnich <kjaine@gmail.com>
2025-11-01 13:18:55 +0200

    data

--- src/data/standingsU16.js
+++ src/data/standingsU16.js
@@ -1,7 +1,7 @@
 export const standingsU16 = [
     { place: 0, team: 'МДЮСШ Ягуари, м. Дніпро',      games: 2, zsh: 0,  psh: 0, vi: 0, pi: 0, pts: 0 },
     { place: 0, team: 'Дніпро, м. Херсон',       games: 2, zsh: 0,  psh: 0, vi: 0, pi: 0, pts: 0 },
-    { place: 0, team: 'ДЮСШ Патріот, м. Вінниця',     games: 2, zsh: 0, psh: 0,  vi: 0, pi: 0, pts: 0 },
+    { place: 0, team: 'ДЮСШ Патріот, м. Вінниця',     games: 2, zsh: 0, psh: 0,  vi: 2, pi: 0, pts: 6 },
     { place: 0, team: 'ДЮСШ Авангард, м. Кременчук',  games: 2, zsh: 0,  psh: 0, vi: 0, pi: 0, pts: 0 }, 
     { place: 0, team: 'ДЮСШ Сокіл - 2010, м. Київ',   games: 2, zsh: 0, psh: 0,  vi: 0, pi: 0, pts: 0 },
     { place: 0, team: 'ДЮСШ Сокіл - 2011, м. Київ',   games: 2, zsh: 0, psh: 0,  vi: 0, pi: 0, pts: 0 },
